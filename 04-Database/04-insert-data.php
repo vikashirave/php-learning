baff6bd61a9ee064c9b9b0246f46ddf70356cc98
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2020-06-25 16:47:26 +0530

    edit

---
+++ 04-Database/04-insert-data.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost:3308";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO customer (firstname, lastname, email)
+VALUES ('Vikas', 'Hirave', 'vikashirave@gmail.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
