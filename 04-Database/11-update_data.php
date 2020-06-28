c47ba25d52526c20282dabece6009980fcc767e0
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2020-06-28 23:42:09 +0530

    commit

---
+++ 04-Database/11-update_data.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost:3308";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE customer SET lastname='Doe' WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
